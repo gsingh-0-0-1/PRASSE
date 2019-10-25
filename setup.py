d1604f6afafe871f47192ab6bb4549df35ed1a57
Gurmehar Singh <gurmehar@gmail.com>
2019-10-25 09:14:23 -0700

    Added setup.py to auto-create necessary directories to function properly.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#* Copyright (C) Gurmehar Singh - All Rights Reserved
+#* Unauthorized copying of this file, via any medium is strictly prohibited
+#* Proprietary and confidential
+#* Written by Gurmehar Singh <gurmehar@gmail.com>, October 2019
+#*/
+
+'''This script will make any directories needed to get the main.py file
+up and running'''
+
+import os
+
+contents = os.listdir()
+
+dirs_to_make = ['images', 'pulsar', 'not_pulsar']
+
+for d in dirs_to_make:
+    if d not in contents:
+        os.mkdir(d)
