21a50bd12d87539509de7c4ac252421785eed82f
Gurmehar Singh <gurmehar@gmail.com>
2019-11-15 16:01:11 -0800

    Separated CNN essentials definitions into a separate file for future compatibility.

---
+++ cnnstructure.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+#********#********#********#********#********#********#********#
+#********#********#CLASS DEFINITIONS#********#********#********#
+#********#********#********#********#********#********#********#
+
+class neuron:
+    def __init__(self, thresh):
+        self.thresh = thresh
+        self.val = None
+
+    def reset(self):
+        self.val = None
+
+    def inp(self, val):
+        self.val = val
+
+    def pval(self):
+        print(self.val)
+
+class layer:
+    def __init__(self, shape):
+        self.shape = shape
+        self.struc = np.zeros(shape)
+        
+    def regenerate(self):
+        del self.struc
+        self.struc = np.zeros(shape)
+
+    def inp(self, arr):
+        if np.shape(arr) == np.shape(self.struc):
+            pass
+        else:
+            raise Exception("Shape of input is not same as shape of layer.")
+
+        self.struc = arr
+
+    def genthresh(self, thresh):
+        self.thresh = np.full(self.shape, thresh)
+            
+        
+def passvals(layer1, layer2, transformation): #layer1, layer2, then a list of transformations to find the index to pass the value to
+    if len(transformation) == len(np.shape(layer1)):
+        pass
+    else:
+        raise Exception("Length of transformations is not equal to number of dimensions in first array.")
+    
+    for idx in np.ndindex(np.shape(layer1.struc)):
+        newind = []
+        for ind in range(len(idx)):
+            if transformation[ind] == None:
+                continue
+            else:
+                newind += [int( eval( str(idx[ind]) + transformation[ind] ) )]
+
+        newind = tuple(newind)
+        if layer1.struc[idx] > layer1.thresh[idx]:
+            layer2.struc[newind] += layer1.struc[idx]
+            
+        
+
+#********#********#********#********#********#********#********#
